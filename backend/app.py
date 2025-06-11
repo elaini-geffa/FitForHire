f735eda6d31751541075788931b117cd06e5e6f5
elainigeffa <elaini@vt.edu>
2025-06-11 00:30:14 -0400

    Accept  resume and job pdf

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,28 @@
+from flask import Flask, request, jsonify
+import pdfplumber
 
+app = Flask(__name__)
+
+@app.route('/analyze', methods = ['POST'])
+
+def analyze():
+    resume_pdf = request.files['resume']
+    job_pdf = request.files['job']
+
+    with pdfplumber.open(resume_pdf) as pdf:
+        resume_text = ""
+        for page in pdf.pages:
+            resume_text += page.extract_text()
+
+    with pdfplumber.open(job_pdf) as pdf:
+        job_text = ""
+        for page in pdf.pages:
+            job_text += page.extract_text()
+
+    return jsonify({
+        'resume_text': resume_text[:300], 
+        'job_text': job_text[:300]
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
