06b772d3d8a2be38c298c69b2eb135e841802246
elainigeffa <elaini@vt.edu>
2025-06-11 18:48:00 -0400

    AI

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, jsonify
+
 import pdfplumber
+
+from utils import clean_and_tokenize
+from utils import extract_keywords
+from utils import get_resume_feedback
 
 app = Flask(__name__)
 
@@ -19,9 +24,30 @@ def analyze():
         for page in pdf.pages:
             job_text += page.extract_text()
 
+    resume_words = clean_and_tokenize(resume_text)
+    keywords = extract_keywords(job_text)
+    matched_keywords = []
+    missing_keywords = []
+    total = len(keywords)
+    total_matched = len(matched_keywords)
+    score = 0
+
+    for keyword in keywords:
+        if keyword in resume_words:
+            matched_keywords.append(keyword)
+            total_matched += 1
+        else:
+            missing_keywords.append(keyword)
+
+    score += total_matched / total * 100
+
+    feedback = get_resume_feedback(resume_text, job_text)
+
     return jsonify({
-        'resume_text': resume_text[:300], 
-        'job_text': job_text[:300]
+        'match_score': str(score) + '%', 
+        'matched_keywords': matched_keywords,
+        'missing_keywords': missing_keywords,
+        'ai_feedback': feedback
     })
 
 if __name__ == '__main__':
